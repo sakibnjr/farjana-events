970715de3a1f2a762c2608a236e5716d03e648d6
sakibnjr <sakibnjr@proton.me>
2025-05-28 17:28:00 +0600

    testing custom font

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Tangerine:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
